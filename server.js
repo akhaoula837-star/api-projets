87ccfe757f40461c4e34a3836032964064785b9f
akhaoula837-star <akhaoula837@gmail.com>
2025-12-02 11:47:36 +0000

    new scripts

---
+++ server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+//const authRoutes = require('./routes/auth');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/api-projets')
+.then(() => console.log("✅ MongoDB Connected"))
+.catch(err => console.log("❌ MongoDB Error:", err));
+
+// Routes
+//app.use('/api/auth', authRoutes);
+
+//app.get('/', (req, res) => {
+//  res.send('API Running...');
+//});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log('🚀 Server running on port ${PORT}');
+});
