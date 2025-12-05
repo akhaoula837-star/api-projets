a7b31c4c6f380c59f9c6ad72ef067997a6be1201
akhaoula837-star <akhaoula837@gmail.com>
2025-12-05 12:53:22 +0000

    new changes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const {
+  register,
+  login,
+  getProfile,
+} = require("../controllers/userController");
+const auth = require("../middleware/authMiddleware");
+
+// Inscription
+router.post("/register", register);
+
+// Connexion
+router.post("/login", login);
+
+// Profil (token requis)
+router.get("/me", auth, getProfile);
+
+module.exports = router;
 
--- server.js
+++ server.js
@@ -3,24 +3,31 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-//const authRoutes = require('./routes/auth');
-
+const authRoute = require('./routes/authRoutes');
+const projectRoute = require('./routes/projectRoutes');
+const taskRoute = require('./routes/taskRoutes');
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
+
 mongoose.connect('mongodb://localhost:27017/api-projets')
-.then(() => console.log("✅ MongoDB Connected"))
-.catch(err => console.log("❌ MongoDB Error:", err));
+.then(() => console.log(" MongoDB Connected"))
+.catch(err => console.log(" MongoDB Error:", err));
+
 
 // Routes
-//app.use('/api/auth', authRoutes);
+app.use('/api/users', authRoute);
+app.use('/api/projects', projectRoute);
+app.use('/api/tasks', taskRoute);
 
-//app.get('/', (req, res) => {
-//  res.send('API Running...');
-//});
+// Route test
+app.get('/', (req, res) => {
+  res.send('API ISIDS en fonctionnement...');
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log('🚀 Server running on port ${PORT}');
+  console.log(' Server running on port ${PORT}');
 });
